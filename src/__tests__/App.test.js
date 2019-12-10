0209a1462594171671fe6539d4cf468b7b0c0dc4
Harvtronix Webmaster <webmaster@harvtronix.com>
2019-12-10 04:14:47 -0500

    Updating node modules - Adding ci npm scripts - Fixing unit tests

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import App from '../client/components/App/App'
+import App from '~/client/App'
 
 it('renders without crashing', () => {
     const div = document.createElement('div')
