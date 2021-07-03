23b824452b1621b7e460e55ad96ec41fcae619f9
Harvtronix Webmaster <webmaster@harvtronix.com>
2021-07-02 23:03:25 -0400

    dimmer indicator and switch updates
    - adding loading spinner while waiting for device db update after toggling a device
    - loading spinner works for switches, lights, and locks
    - updating brightness feature indicator value
    - toggleDevicePowerState action now uses either /turn_on or /turn_off instead of /toggle

--- src/client/components/_shared/DeviceTile/indicators/PowerIndicator.jsx
+++ src/client/components/_shared/DeviceTile/indicators/PowerIndicator.jsx
@@ -1,10 +1,12 @@
 import MoonIcon from '@carbon/icons-react/es/asleep/32'
 import QuestionMarkIcon from '@carbon/icons-react/es/help/32'
 import SunIcon from '@carbon/icons-react/es/light/32'
+import { InlineLoading } from 'carbon-components-react'
 import PropTypes from 'prop-types'
 import React from 'react'
 import { useSubstate } from 'react-substate'
 
+import Constants from '~/client/modules/Constants'
 import actions from '~/client/modules/substate/actions'
 import substates from '~/client/modules/substate/substates'
 
@@ -14,10 +16,12 @@ const PowerIndicator = (props) => {
     const [, dispatch] = useSubstate(substates.deviceDb)
     const getIcon = () => {
         switch (props.state) {
-        case 'on':
+        case Constants.DeviceStates.ON:
             return <SunIcon className={styles.icon} />
-        case 'off':
+        case Constants.DeviceStates.OFF:
             return <MoonIcon className={styles.icon} />
+        case Constants.DeviceStates.LOADING:
+            return <InlineLoading className={styles.loadingSpinner} />
         default:
             return <QuestionMarkIcon className={styles.icon} />
         }
